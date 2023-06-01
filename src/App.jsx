a55d4f030de744e29894003971967e9f59d75789
Kabina Thapa <thapakabina28@gmail.com>
2023-06-01 14:55:59 -0400

    Handling promise Rejection

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@
 import "./App.css";
 
 function App() {
-  //Fetch function to make a GET request to chosen API endpoint.
+  //1. Make API request and handle promise
 
+  //Fetch function to make a GET request to chosen API endpoint.
   const dataFetch = fetch("https://jsonplaceholder.typicode.com/posts");
-  console.log(data);
+  console.log(dataFetch);
 
   //Handling the promise using then() and catch() methods; hanldes the response or any errors that may occur.
 
@@ -14,7 +15,7 @@ function App() {
 
   dataFetch
     .then((response) => response.json()) //response.json() method is called to parse response body as JSON.
-    //It returns a promise that resolves to parsed JSON data.
+                                         //It returns a promise that resolves to parsed JSON data.
 
     .then((data) => {                    //The 2nd then() method is chained to promise returned by response.json().
       console.log(data)
@@ -26,7 +27,23 @@ function App() {
       console.error("Error", error)
     }
     ) 
-//
+//2. Handle Promise Rejection
+const wrongdataFetch = fetch("https://jsonplaceholder.typicode.com/pot");
+console.log(wrongdataFetch);
+wrongdataFetch
+    .then(response => {
+      if(!response.ok){   //this condition will check if there is any error while fetching url. If there is no error it will go to return (response.json). 
+        console.log("Error; either Network issues or the url doesn't exist.")
+      }
+     return response.json()
+    })
+    .then((data) => {                   
+      console.log(data)
+    })
+    .catch((error)=>{ //hadles promiseRejection
+      console.log('You have encountered Errors', error)
+    }
+    ) 
 
   return <></>;
 }
